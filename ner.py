d9f2935fa48e255182dcb3caaa7a225dd8d00c40
jamespatten719 <jamespatten1996@gmail.com>
2018-04-02 11:07:08 +0100

    Update ner.py

--- ner.py
+++ ner.py
@@ -4,6 +4,30 @@ Created on Wed Mar 28 13:38:02 2018
 
 @author: jamespatten
 """
+import cgi
+import cgitb; cgitb.enable()  # for troubleshooting
+print("Content-type: text/html")
+print """
+  <!DOCTYPE html>
+<head>
+	<meta charset="utf-8">
+	<title>NER CHUNKER</title>
+	<style>
+
+	</style>
+</head>
+
+<body>
+	<p id="ner"></p>
+	<script>
+	
+	var sentence = prompt("Please enter a sentence", "E.g. Sarah works for Deloitte");
+	document.getElementById("ner").innerHTML = sentence;
+	
+	</script> 
+</body>
+"""
+form = cgi.FieldStorage()
 
 import nltk
 nltk.download('maxent_ne_chunker')
@@ -12,8 +36,9 @@ nltk.download('words')
 from nltk import word_tokenize, pos_tag, ne_chunk
 
 if __name__ == "__main__":
+    
 
-	sentence = "Sarah works for Deloitte"
+	sentence = form.getvalue('ner')
 	tokens = nltk.word_tokenize(sentence)
 	print("tokens = %s" % tokens)
 
