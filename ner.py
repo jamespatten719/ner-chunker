41efb2bcadcf65c789adf8c1efa500c86398cdea
jamespatten719 <jamespatten1996@gmail.com>
2018-04-07 21:14:46 +0000

    cgi file for browser

--- ner.py
+++ ner.py
@@ -1,48 +1,20 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Mar 28 13:38:02 2018
-
-@author: jamespatten
-"""
+#!/usr/bin/python3
 import cgi
-import cgitb; cgitb.enable()  # for troubleshooting
-print("Content-type: text/html")
-print """
-  <!DOCTYPE html>
-<head>
-	<meta charset="utf-8">
-	<title>NER CHUNKER</title>
-	<style>
-
-	</style>
-</head>
-
-<body>
-	<p id="ner"></p>
-	<script>
-	
-	var sentence = prompt("Please enter a sentence", "E.g. Sarah works for Deloitte");
-	document.getElementById("ner").innerHTML = sentence;
-	
-	</script> 
-</body>
-"""
+import cgitb 
+cgitb.enable()
+print("Content-type:text/html\r\n\r\n")
+print("<html><body>")
+print("<h1>Named Entity Recognition </h1>")
 form = cgi.FieldStorage()
+if form.getvalue("sentence"):
+        sentence = form.getvalue("sentence")
+	print("<h1>Hello "+sentence+"! Thanks for using my script!</h1><br />")
+print("<form method='post' action='ner.py'>")
+print("<p>Please enter a sentence:  <input type='text' name='sentence'/></p>")
+print("<input type='submit' value='Submit' />")
+print("</form>")
+print("</body></html>")
+                        
 
-import nltk
-nltk.download('maxent_ne_chunker')
-nltk.download('words')
 
-from nltk import word_tokenize, pos_tag, ne_chunk
 
-if __name__ == "__main__":
-    
-
-	sentence = form.getvalue('ner')
-	tokens = nltk.word_tokenize(sentence)
-	print("tokens = %s" % tokens)
-
-	tagged = nltk.pos_tag(tokens)
-
-	entities = nltk.chunk.ne_chunk(tagged)
-	print("entities = %s" % entities)
